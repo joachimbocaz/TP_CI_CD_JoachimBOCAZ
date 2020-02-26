ddd0bc28c1dbdbed470ebbd3a4a38690d746d2e9
joachimbocaz <apple@MacBook-Pro.local>
2020-02-26 16:54:55 +0100

    modif code test pour que ç ame

--- packages/core/tasks/test/unsupported-features.js
+++ packages/core/tasks/test/unsupported-features.js
@@ -4,7 +4,7 @@ const log = require('bestikk-log')
 log.task('Check unsupported features')
 const data = fs.readFileSync('build/asciidoctor-browser.js', 'utf8')
 const mutableStringPattern = /\['\$(g)?sub!'\]/
-if (!mutableStringPattern.test(data)) {
+if (mutableStringPattern.test(data)) {
   log.error('Mutable String methods are not supported in Opal, please replace sub! and gsub! methods')
   process.exit(1)
 }
